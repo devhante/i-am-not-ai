189369ed9eae8dea4375c113dda8f25d38c95718
Kyungwook Cho <amisay5256@naver.com>
2018-06-11 12:26:57 +0900

    add joystick

---
+++ i-am-not-ai/Assets/Scripts/UI/PlayMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMenu : Menu<PlayMenu>
+{
+    public Button buttonSettings;
+    public Button buttonAttack;
+	public Joystick joystick;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		buttonSettings.onClick.AddListener(() => { SettingsMenu.Open();});
+	}
+}
