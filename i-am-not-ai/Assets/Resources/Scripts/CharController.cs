50d6bba3cf6b1c9d6efb7f10de488ed101a3885d
ISHNN <ishnn032n@gmail.com>
2018-06-11 12:18:42 +0900

    UpdateUnityChan

--- i-am-not-ai/Assets/Resources/Scripts/CharController.cs
+++ i-am-not-ai/Assets/Resources/Scripts/CharController.cs
@@ -37,7 +37,13 @@ public class CharController : MonoBehaviour
         float input_h = Input.GetAxis("Horizontal");
         float input_v = Input.GetAxis("Vertical");
         anim.SetFloat("Speed", input_v);
+
+        //혹시 옆으로만 가고 있을 때에도 애니에 속도를 넣어줘야 해서.(수정예정)
+        if(Mathf.Abs(input_v) <= 0.1f)
+            anim.SetFloat("Speed", Mathf.Abs(input_h));
+        
         anim.SetFloat("Direction", input_h);
+
         anim.speed = animSpeed;
 
         //애니매이션 상태 초기화
@@ -51,17 +57,24 @@ public class CharController : MonoBehaviour
         //velocity = camera.TransformDirection(velocity);
         //다음과 같이 카메라에 맞추어 수정하는 것이 적합하다고 본다
 
-        //v입력 임계에 따라 속도의 변화를 준다
-        if(input_v > 0.1f || input_h > 0.1f)
+        //입력에 따라 속도의 변화를 준다
+        if (Mathf.Abs(input_v) > 0.1f || Mathf.Abs(input_h) > 0.1f)
         {
-            if(Mathf.Abs(input_h) > 0.1f)
+            //뒤로 가고 있으면 어찌 되었건 뒤로가는 속도가 나오는 것으로 했다
+            if (input_v < -0.1f)
+                velocity *= backwardSpeed;
+
+            //만약 옆으로 움직인다면
+            else if (Mathf.Abs(input_h) > 0.1f)
             {
                 //앞과 옆의 비율을 계산하여 옆속도 ~ 앞속도 만큼 움직인다
                 velocity *= Mathf.Lerp(sideSpeed, forwardSpeed, input_v / (1 + Mathf.Abs(input_h)));
             }
+
+            //앞으로만 가고 있다면
             else
             {
-                //앞을 향하는 임계에 있을 경우 달리기를 체크한다
+                //앞을 향하는 임계에 있을 경우만 달리기를 체크한다
                 if (Input.GetKey(KeyCode.LeftShift))
                 {
                     anim.SetBool("Run", true);
@@ -72,15 +85,36 @@ public class CharController : MonoBehaviour
                     velocity *= forwardSpeed;
             }
         }
-        //뒤로 가고 있으면 어찌 되었건 뒤로가는 속도가 나오는 것으로 했다
-        else if (input_v < -0.1f || input_h < -0.1f)
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            velocity *= backwardSpeed;
+            rigib.AddForce(Vector3.up, ForceMode.Impulse);
+            anim.SetBool("Jump",true);
         }
+        */
 
         //연산이 끝난 벡터를 로컬포지션에서 더한다
         transform.localPosition += velocity * Time.fixedDeltaTime;
 
         #endregion Move
     }
+
+    /*
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag.Equals("Platform"))
+        {
+            this.transform.parent = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag.Equals("Platform"))
+        {
+            this.transform.parent = null;
+        }
+    }
+    */
 }
