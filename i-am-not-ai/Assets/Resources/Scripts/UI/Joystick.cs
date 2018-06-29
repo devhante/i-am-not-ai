0e700203e81e9e161d2d9c164097d58f3cc5bb8c
Kyungwook Cho <amisay5256@naver.com>
2018-06-29 23:48:37 +0900

    UI WASD로 움직이도록 수정

--- i-am-not-ai/Assets/Resources/Scripts/UI/Joystick.cs
+++ i-am-not-ai/Assets/Resources/Scripts/UI/Joystick.cs
@@ -16,6 +16,28 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         radius = GetComponent<RectTransform>().sizeDelta.x * 0.5f;
     }
 
+    private void Start()
+    {
+        StartCoroutine("ChangeControllerPositionCoroutine");
+    }
+
+    int xAxis = 0;
+    int yAxis = 0;
+
+    private void Update()
+    {
+        if (name == "Joystick")
+        {
+            if (Input.GetKey(KeyCode.D)) xAxis = 1;
+            else if (Input.GetKey(KeyCode.A)) xAxis = -1;
+            else xAxis = 0;
+
+            if (Input.GetKey(KeyCode.W)) yAxis = 1;
+            else if (Input.GetKey(KeyCode.S)) yAxis = -1;
+            else yAxis = 0;
+        }
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         StartCoroutine("PointerDown");
@@ -36,12 +58,36 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             Vector3 mousePos = Input.mousePosition;
 
             float distance = Vector3.Distance(centerPos, mousePos);
-            if (distance > 64) mousePos = centerPos + Vector3.ClampMagnitude((mousePos - centerPos), radius);
+            if (distance > radius) mousePos = centerPos + Vector3.ClampMagnitude((mousePos - centerPos), radius);
             controller.position = mousePos;
 
             Normal = Vector3.Normalize(mousePos - centerPos);
 
             yield return null;
+        }
+    }
+
+    private IEnumerator ChangeControllerPositionCoroutine()
+    {
+        while (true)
+        {
+            Vector3 centerPos = transform.position;
+            Vector3 controllerPos = controller.position;
+            Vector3 destination = centerPos + new Vector3(xAxis, yAxis).normalized * radius;
+            Vector3 destinationFrame = controllerPos + (destination - controllerPos).normalized * radius * 4 * Time.deltaTime;
+            float distance = Vector3.Distance(centerPos, destinationFrame);
+
+            Debug.Log((destination - destinationFrame).magnitude);
+
+            if (distance != 0)
+            {
+
+                if (distance > radius) destinationFrame = centerPos + Vector3.ClampMagnitude((destinationFrame - centerPos), radius);
+                if ((destination - destinationFrame).magnitude <= 10.0f) destinationFrame = destination;
+                controller.position = destinationFrame;
+            }
+
+            yield return new WaitForEndOfFrame();
         }
     }
 }
